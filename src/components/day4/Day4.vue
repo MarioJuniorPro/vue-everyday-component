25a02115bb69ab56401ceacbadf16797ae9cc849
MarioJuniorPro <mario.junior.pro@gmail.com>
2017-04-08 03:47:06 -0300

    day 4

--- src/components/day4/Day4.vue
+++ src/components/day4/Day4.vue
@@ -9,6 +9,21 @@ import Vue from 'vue'
 // import ScrollTop from './ScrollTop.vue'
 
 // Vue.component(ScrollTop.name, ScrollTop)
+/*https://api.github.com/users/dhh/repos?sort=pushed&per_page=2000&access_token=189237662398c3cdd5eb036e94b127749fb0fb30
+  {
+    1:,{
+      1:,
+      3:,
+    }
+    2:,
+    3:,
+
+  }
+*/
+
+Vue.$http('https://api.github.com/users/dhh/repos?sort=pushed&per_page=2000&access_token=189237662398c3cdd5eb036e94b127749fb0fb30').then((d)=>{
+  console.log(d.length)
+})
 
 export default {
   name: 'day4',
@@ -22,3 +37,5 @@ export default {
 <style>
 
 </style>
+
+
